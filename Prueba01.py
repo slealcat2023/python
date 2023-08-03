02414c4fb80ed4f603226a6191f3fb73784bc190
Sergio Leal <slealcat@gmail.com>
2023-08-03 09:53:09 -0600

    probe el uso del case

--- Prueba01.py
+++ Prueba01.py
@@ -17,15 +17,18 @@ print("") #Este es un comentario
 
 vuelta = 1
 while vuelta <= 10:
-    print("Vuelta "+str(vuelta))
-    
-    if int((vuelta<2)):
-        print("Sergio")
-    elif int((vuelta==2)):
-            print("Mi kike")
-    elif int((vuelta==3)):
-            print("Mi Marquitos")
-    else:
-        print("el resto del mundo")
+    #print("Vuelta "+str(vuelta))
+    match vuelta:
+        case 1:
+            print("Sergio")
+        case 2:
+            print("Kike") 
+        case 3:
+            print("Marquitos")
+        case other:
+            print ("No match")
 
     vuelta = vuelta + 1
+
+
+          
